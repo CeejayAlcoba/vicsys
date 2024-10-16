9061bd1ee1aec5f71d836b7d87c1724f00334e4b
CeejayAlcoba <smithkim909@gmail.com>
2024-10-16 20:47:48 +0800

    fix router

--- src/pages/routes/AppRoute.tsx
+++ src/pages/routes/AppRoute.tsx
@@ -14,6 +14,7 @@ export default function AppRoute() {
   const { user } = useUserContext();
   const router = createBrowserRouter([
     {
+      element: user && <Navigate to="/" />,
       children: [
         {
           path: "login",
