a317e7d971d5c8990ac68d5d43c0a7171e56c4e4
CeejayAlcoba <smithkim909@gmail.com>
2024-11-11 20:04:28 +0800

    update mykids make it view only

--- src/pages/private/my-kids/MyKidsPage.tsx
+++ src/pages/private/my-kids/MyKidsPage.tsx
@@ -7,14 +7,14 @@ import IChild from "../../../interfaces/firebase/IChild";
 import {
   CheckCircleOutlined,
   CloseCircleOutlined,
-  DeleteOutlined,
-  EditOutlined,
-  PlusOutlined,
+  // DeleteOutlined,
+  // EditOutlined,
+  // PlusOutlined,
 } from "@ant-design/icons";
-import MyKidSaveModal from "./modal/MyKidSaveModal";
-import { useState } from "react";
-import { Button, Form } from "antd";
-import Swal from "sweetalert2";
+// import MyKidSaveModal from "./modal/MyKidSaveModal";
+// import { useState } from "react";
+// import { Button, Form } from "antd";
+// import Swal from "sweetalert2";
 
 export default function MyKidsPage() {
   const { user } = useUserContext();
@@ -23,9 +23,9 @@ export default function MyKidsPage() {
     queryKey: ["children"],
     queryFn: async () => await _childrenService.getByUserId(user?.uid ?? ""),
   });
-  const [form] = Form.useForm();
-  const [isSaveModalVisible, setIsSaveModalVisible] = useState<boolean>(false);
-  const [selectedChild, setSelectedChild] = useState<IChild | null>(null);
+  // const [form] = Form.useForm();
+  // const [isSaveModalVisible, setIsSaveModalVisible] = useState<boolean>(false);
+  // const [selectedChild, setSelectedChild] = useState<IChild | null>(null);
   const columns: ColumnsType<IChild> = [
     {
       title: "Name",
@@ -67,59 +67,59 @@ export default function MyKidsPage() {
       ),
     },
 
-    {
-      title: "Actions",
-      width: 600,
-      render: (data: IChild) => (
-        <>
-          <Button
-            type="primary"
-            danger
-            shape="circle"
-            icon={<DeleteOutlined />}
-            onClick={() => {
-              setSelectedChild(data);
-              handleDeleteConfirmation();
-            }}
-          />
-          <Button
-            type="primary"
-            shape="circle"
-            icon={<EditOutlined />}
-            style={{ marginLeft: 8 }}
-            onClick={() => {
-              form.setFieldsValue(data);
-              setSelectedChild(data);
-              setIsSaveModalVisible(true);
-            }}
-          />
-        </>
-      ),
-    },
+    // {
+    //   title: "Actions",
+    //   width: 600,
+    //   render: (data: IChild) => (
+    //     <>
+    //       <Button
+    //         type="primary"
+    //         danger
+    //         shape="circle"
+    //         icon={<DeleteOutlined />}
+    //         onClick={() => {
+    //           setSelectedChild(data);
+    //           handleDeleteConfirmation();
+    //         }}
+    //       />
+    //       <Button
+    //         type="primary"
+    //         shape="circle"
+    //         icon={<EditOutlined />}
+    //         style={{ marginLeft: 8 }}
+    //         onClick={() => {
+    //           form.setFieldsValue(data);
+    //           setSelectedChild(data);
+    //           setIsSaveModalVisible(true);
+    //         }}
+    //       />
+    //     </>
+    //   ),
+    // },
   ];
-  const handleDeleteConfirmation = () => {
-    Swal.fire({
-      icon: "warning",
-      title: "Are you sure you want to delete?",
-      showCancelButton: true,
-      confirmButtonText: "Delete",
-      confirmButtonColor: "red",
-    }).then(async (result) => {
-      if (result.isConfirmed && selectedChild?.id) {
-        await _childrenService.deleteById(selectedChild.id);
-        refetch();
-        Swal.fire({
-          icon: "success",
-          title: "Successfully deleted",
-          showConfirmButton: false,
-          timer: 1500,
-        });
-      }
-    });
-  };
+  // const handleDeleteConfirmation = () => {
+  //   Swal.fire({
+  //     icon: "warning",
+  //     title: "Are you sure you want to delete?",
+  //     showCancelButton: true,
+  //     confirmButtonText: "Delete",
+  //     confirmButtonColor: "red",
+  //   }).then(async (result) => {
+  //     if (result.isConfirmed && selectedChild?.id) {
+  //       await _childrenService.deleteById(selectedChild.id);
+  //       refetch();
+  //       Swal.fire({
+  //         icon: "success",
+  //         title: "Successfully deleted",
+  //         showConfirmButton: false,
+  //         timer: 1500,
+  //       });
+  //     }
+  //   });
+  // };
   return (
     <>
-      <MyKidSaveModal
+      {/* <MyKidSaveModal
         form={form}
         isOpen={isSaveModalVisible}
         setIsOpen={setIsSaveModalVisible}
@@ -137,7 +137,7 @@ export default function MyKidsPage() {
         }}
       >
         Add Kid
-      </Button>
+      </Button> */}
       <DataTable dataSource={children} columns={columns} />
     </>
   );
